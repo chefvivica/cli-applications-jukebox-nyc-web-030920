dd855893db36aa0ced821e5a17e4e3aa8761a5fa
Vivica Ma <vivicacongma@gmail.com>
2020-02-28 00:13:46 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -74,4 +74,24 @@ def run(songs)
 # end 
 # run(songs)
     
-  
+    command = "" 
+  while command  
+  puts "Please enter a command:"
+  command = gets.downcase.strip  
+  case command 
+    when 'list'
+      list(songs)
+      when 'play'
+        list(songs)
+        play(songs)
+      when 'help'
+        help 
+      when 'exit'
+        exit_jukebox
+        break 
+      else 
+        help 
+      end 
+    end 
+  end 
+  run(songs)
